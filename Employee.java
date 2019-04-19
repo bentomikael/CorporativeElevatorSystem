9d4ad5ffadd3699ee1e6dc06621334bf5538db19
Mikael Bento <43865953+bentomikael@users.noreply.github.com>
2019-04-18 23:27:40 -0300

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,51 @@
+package sistemaelevadorcorporativo;
+
+/**
+ *
+ * @author Acer
+ */
+public abstract class Employee implements People {
+    private final int codeAccess;
+    private int levelAccess;
+    private int currentFloor;
+    private final String name;
+    private int age;
+    private final String gender;
+    private int daniedAccessAttempts;
+
+    public Employee(int codeAccess, int levelAccess, String name, int age, String gender) {
+        this.codeAccess = codeAccess;
+        this.levelAccess = levelAccess;
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        currentFloor = 0;
+        daniedAccessAttempts = 0;
+    }
+
+
+    @Override
+    public String getName() {return name;}
+
+    @Override
+    public int getAge() {return age;}
+    
+    public void setAge(int age){this.age = age;}
+
+    @Override
+    public String gender() {return gender;}
+    
+    public int getCodeAccess() {return codeAccess;}
+
+    public int getLevelAccess() {return levelAccess;}
+
+    public void setLevelAccess(int levelAccess) {this.levelAccess = levelAccess;}
+
+    public int getCurrentFloor() {return currentFloor;}
+
+    public void setCurrentFloor(int currentFloor) {this.currentFloor = currentFloor;}
+    
+    public int getDaniedAccessAttempts(){return daniedAccessAttempts;}
+    
+     
+}
---
+++ People.java
@@ -0,0 +1,12 @@
+package sistemaelevadorcorporativo;
+
+/**
+ *
+ * @author Acer
+ */
+public interface People {
+    public String getName();
+    public int getAge();
+    public String gender();
+    
+}
