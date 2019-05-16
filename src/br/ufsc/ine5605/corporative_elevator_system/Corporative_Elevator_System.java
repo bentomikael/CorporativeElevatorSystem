1294696a266f8d33b7fba39c86206f289643fcf1
Decio <Decio@192.168.25.47>
2019-05-16 15:27:19 -0300

    Adequado as convençoes de nome para pacotes

---
+++ src/br/ufsc/ine5605/corporative_elevator_system/Corporative_Elevator_System.java
@@ -0,0 +1,16 @@
+package br.ufsc.ine5605.corporative_elevator_system;
+
+import br.ufsc.ine5605.CorporativeElevatorSystem.Controller.MainControl;
+
+/**
+ *
+ * @author Mikael Bento e Vinicius Hilbert
+ */
+public class Corporative_Elevator_System {
+    public static void main(String args[]){
+        new MainControl().start();
+    }
+    
+    //buscar bugs
+    //fazer 2 try e catch personalizados
+}
